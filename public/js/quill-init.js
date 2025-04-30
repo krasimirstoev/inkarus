802ccb0be0f7323784187ce03ad6e39e6276c1f4
Krasimir Stoev <alabalism@gmail.com>
2025-04-30 18:38:50 +0300

    Adding a timer to re-run highlighting every 60 seconds. In this once per minute new writen text will be scaned and the characters will be highlighted.

--- public/js/quill-init.js
+++ public/js/quill-init.js
@@ -1,7 +1,9 @@
+// public/js/quill-init.js
+
 import { initEditor } from './quill/init/editor.js';
-import { initAutosave }   from './quill/init/autosave.js';
-import { initModes }      from './quill/init/modes.js';
-import { initNotes }      from './quill/init/notes.js';
+import { initAutosave } from './quill/init/autosave.js';
+import { initModes } from './quill/init/modes.js';
+import { initNotes } from './quill/init/notes.js';
 import { initCharacterHighlighter } from './pages/character-highlighter.js';
 
 document.addEventListener('DOMContentLoaded', async () => {
@@ -12,15 +14,21 @@ document.addEventListener('DOMContentLoaded', async () => {
   initNotes(projectId);
 
   try {
-    // 2 Fetch our JSON list of {id, name, pseudonym}
-    const res  = await fetch(`/characters/${projectId}/json-list`);
+    // 2 Fetch our JSON list of { id, name, pseudonym } for this project
+    const res = await fetch(`/characters/${projectId}/json-list`);
     const data = await res.json();
     if (!data.success) throw new Error('Server returned failure');
 
     console.log('[INIT] Characters for highlighter:', data.characters);
 
-    // 3 Hand off to the highlighter
+    // 3 Perform initial highlighting
     initCharacterHighlighter(quill, data.characters, projectId);
+
+    // 4 Set up a timer to re-run highlighting every 60 seconds
+    setInterval(() => {
+      console.log('[INIT] Re-running character highlighter');
+      initCharacterHighlighter(quill, data.characters, projectId);
+    }, 60 * 1000);
 
   } catch (err) {
     console.error('❌ Error loading characters for highlighter:', err);
