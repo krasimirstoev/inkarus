568d53de7b7090c4a841a317a18e790ca84d89aa
Krasimir Stoev <alabalism@gmail.com>
2025-08-03 21:14:01 +0300

    Fixing issue with not working buttons in #24. Adding RTL option in Quill toolbar, related with #35.

--- public/js/quill/init/editor.js
+++ public/js/quill/init/editor.js
@@ -1,7 +1,7 @@
 // public/js/quill/init/editor.js
 
 // Import Quill from a CDN-backed ESM bundle
-import Quill from 'https://cdn.skypack.dev/quill@1.3.7';
+import Quill from 'https://cdn.skypack.dev/quill@2.0.3';
 
 // Import and register our custom CharacterBlot, PlaceBlot, and ItemBlot formats
 import './formats/character.js';
@@ -23,12 +23,59 @@ export function initEditor() {
   const projectId = editorForm.dataset.projectId;
   const draftId   = editorForm.dataset.draftId;
 
+  // Define the toolbar options for the Quill editor
+  // This includes our custom formats like CharacterBlot, PlaceBlot, and ItemBlot
+  // as well as standard formatting options like bold, italic, and lists
+  const toolbarOptions = [
+  [{ 'header': [1, 2, false] }],        // custom button values
+  ['bold', 'italic', 'underline',],        // toggled buttons
+  [{ list: 'bullet' }],
+  ['blockquote'],
+  [{ 'direction': 'rtl' }],
+  ['clean'], // remove formatting button
+  //['blockquote', 'code-block'],
+  //['link', 'image', 'video', 'formula'],
+
+  //[{ 'header': 1 }, { 'header': 2 }],               // custom button values
+  //[{ 'list': 'ordered'}, { 'list': 'bullet' }, { 'list': 'check' }],
+  //[{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+  //[{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+  //[{ 'direction': 'rtl' }],                         // text direction
+
+  //[{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+  //[{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+
+  //[{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+  //[{ 'font': [] }],
+  //[{ 'align': [] }],
+
+  //['clean']                                         // remove formatting button
+];
+
   // Instantiate Quill on the #quill-editor container, enable our custom formats
   const quill = new Quill('#quill-editor', {
     theme: 'snow',
+    modules: {
+      // toolbar: toolbarOptions, // Uncomment to enable the classic toolbar
+      toolbar: '#toolbar', // Inkarus custom toolbar defined in editor.ejs
+      clipboard: {
+        matchVisual: false // Disable visual matching for pasted content
+      }
+    },
     formats: [
-      'bold', 'italic', 'underline', 'strike', 'blockquote', 'link',
-      'list', 'bullet', 'indent',
+      'header',    // Standard header formats
+      'bold',      // Standard bold format
+      'italic',    // Standard italic format
+      'underline', // Standard underline format
+      'strike',    // Standard strikethrough format
+      'list',      // Standard list formats
+      'blockquote',   // Standard blockquote format
+      //'clean',     // Standard clean format (removes formatting)
+      'direction', // Standard text direction format
+      //'align',     // Standard alignment formats
+      //'link',      // Standard link format
+      //'image',     // Standard image format
+      //'video',     // Standard video format
       'character', // Our custom CharacterBlot
       'place',     // Our custom PlaceBlot
       'item'       // Our custom ItemBlot
